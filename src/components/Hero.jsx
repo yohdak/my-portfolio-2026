37ea1a87b5f2910524c440c0a7e2ad365d9704b1
yohdak <pramudya760@gmail.com>
2026-03-03 13:00:23 +0700

    ganti resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Hero.css';
 import { FaInstagram, FaBehance, FaLinkedin } from "react-icons/fa"; // Pake ini, jauh lebih bersih
-import { Download } from 'lucide-react'; 
+import { Download } from 'lucide-react';
 
 const Hero = ({ data }) => {
   return (
@@ -10,10 +10,10 @@ const Hero = ({ data }) => {
         <div className="hero-img">
           <img src={data.avatar} alt={data.name} className="hero-img-1" />
         </div>
-        
+
         <div className="hero-text">
           <h1>{data.name}</h1>
-          
+
           {/* --- BAGIAN SOSMED (BERSIH) --- */}
           <div className="social-links">
             <a href="https://instagram.com/yohdaklah" target="_blank" rel="noopener noreferrer">
@@ -22,11 +22,11 @@ const Hero = ({ data }) => {
             <a href="https://instagram.com/yohdakdesigner" target="_blank" rel="noopener noreferrer">
               <FaInstagram size={24} /> yohdakdesigner
             </a></div>
-           <div className="social-links">
+          <div className="social-links">
             <a href="https://instagram.com/jesril_pratama_" target="_blank" rel="noopener noreferrer">
               <FaInstagram size={24} /> jesril_pratama_
             </a>
-        
+
             <a href="https://behance.net/jesrilpratama" target="_blank" rel="noopener noreferrer">
               <FaBehance size={24} />
             </a>
@@ -42,9 +42,9 @@ const Hero = ({ data }) => {
       <div className="hero-section-bottom">
         <div className="bio-section">
           <p className="bio">{data.bio}</p>
-          <a 
-            href="/cv-pramudya-jesril-pratama.pdf" 
-            download="Pramudya_Jesril_CV.pdf"
+          <a
+            href="/Pramudya Jesril Pratama-resume.pdf"
+            download="Pramudya Jesril Pratama-resume.pdf"
             className="btn-contact"
           >
             <span>Download CV</span>
